8b4628339b2cc214d7dab03ebec581e704952279
Artyom Rodionov <artemr19032006@gmail.com>
2024-04-01 15:14:25 +0300

    Updated splash screen with animation, handling orientation changes

--- app/src/main/java/com/smmanager/MainActivity.kt
+++ app/src/main/java/com/smmanager/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.smmanager
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.webkit.WebViewClient
 import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.smmanager.databinding.ActivityMainBinding
 import com.smmanager.web_view.SMMYaWebViewClient
@@ -15,12 +24,49 @@ class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
+        handleOrientationChanges()
+
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        setupSplashScreenAnimation(splashScreen)
+
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupWebView()
         setupBackPressOverriding()
+    }
+
+    @SuppressLint("SourceLockedOrientationActivity")
+    private fun handleOrientationChanges() {
+        if (!packageManager.hasSystemFeature(PackageManager.FEATURE_SENSOR_HINGE_ANGLE)) {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+    }
+
+    private fun setupSplashScreenAnimation(splashScreen: SplashScreen) {
+        splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
+            val scaleXAnimator = ObjectAnimator.ofFloat(
+                splashScreenViewProvider.iconView, View.SCALE_X, 0.0f, 1f
+            )
+
+            val scaleYAnimator = ObjectAnimator.ofFloat(
+                splashScreenViewProvider.iconView, View.SCALE_Y, 0.0f, 1f
+            )
+
+            val rotationAnimator = ObjectAnimator.ofFloat(
+                splashScreenViewProvider.iconView, View.ROTATION, 0f, 360f
+            )
+            val animatorSet = AnimatorSet()
+            animatorSet.apply {
+                playTogether(scaleYAnimator, scaleXAnimator, rotationAnimator)
+                duration = 800L
+                doOnEnd {
+                    splashScreenViewProvider.remove()
+                }
+                start()
+            }
+        }
     }
 
     private fun setupWebView() {
@@ -33,7 +79,9 @@ class MainActivity : AppCompatActivity() {
             useWideViewPort = true
         }
 
-        binding.webView.webViewClient = SMMYaWebViewClient()
+        binding.webView.webViewClient = SMMYaWebViewClient(
+            onErrorAction = this::handleWebViewErrors
+        )
         binding.webView.loadUrl(BuildConfig.SMMYa_URL)
     }
 
@@ -43,6 +91,22 @@ class MainActivity : AppCompatActivity() {
                 binding.webView.goBack()
             } else {
                 finish()
+            }
+        }
+    }
+
+    private fun handleWebViewErrors(errorCode: Int) {
+        when (errorCode) {
+            WebViewClient.ERROR_HOST_LOOKUP -> {
+                binding.errorText.text = resources.getText(R.string.some_error)
+                binding.errorComponent.visibility = View.VISIBLE
+                binding.webView.visibility = View.GONE
+            }
+
+            else -> {
+                binding.errorText.text = resources.getText(R.string.some_error)
+                binding.errorComponent.visibility = View.VISIBLE
+                binding.webView.visibility = View.GONE
             }
         }
     }
--- app/src/main/java/com/smmanager/web_view/SMMYaWebViewClient.kt
+++ app/src/main/java/com/smmanager/web_view/SMMYaWebViewClient.kt
@@ -3,13 +3,16 @@ package com.smmanager.web_view
 import android.app.Activity
 import android.content.Intent
 import android.webkit.CookieManager
+import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.core.content.ContextCompat
 import com.smmanager.BuildConfig
 
-class SMMYaWebViewClient : WebViewClient() {
+class SMMYaWebViewClient(
+    private val onErrorAction: (Int) -> Unit = {}
+) : WebViewClient() {
 
     override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
         if (request != null && request.url.host == BuildConfig.SMMYa_URL_HOST) {
@@ -24,5 +27,15 @@ class SMMYaWebViewClient : WebViewClient() {
             }
         }
         return true
+    }
+
+    override fun onReceivedError(
+        view: WebView?,
+        request: WebResourceRequest?,
+        error: WebResourceError?
+    ) {
+        if (error != null) {
+            onErrorAction(error.errorCode)
+        }
     }
 }
